731751c4706d8485613bfbf55cacfe7261a70da5
Eduardo <eduao154@gmail.com>
2026-06-09 10:34:52 -0400

    Adding

--- fronted/src/HomePage.jsx
+++ fronted/src/HomePage.jsx
@@ -21,7 +21,7 @@ function App() {
 
   async function fetchData() {
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_ROUTER}/GetAllUser/info`);
+      const response = await fetch(`${import.meta.env.API_ROUTER}/GetAllUser/info`);
       const data = await response.json();
       setUsers(data.data || []);
       setCurrentPage(1);
